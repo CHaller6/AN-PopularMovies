f93c4a7a348ff1ce8d3c961b4e922d73db5d0f7f
Chris <challer6@gmail.com>
2017-09-16 17:42:28 -0700

    Create new child activity

--- MyApplication/app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.android.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
